9bea6a3131cfb9f8913df1efb6b2b8bbe2d55c67
Erick Díaz <erickdiazc717@gmail.com>
2026-07-17 11:34:40 -0500

    add delay property

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,6 +110,8 @@ const reveal = (selector, options = {}) => {
         scrollTrigger: selector,
         opacity: 0,
         duration: 1,
+        y: 100,
+        delay: .3,
         
     })
 }
