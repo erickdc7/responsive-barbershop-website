223dc0beeb88f89b0aeb160311699a03ac0384a2
Erick Díaz <erickdiazc717@gmail.com>
2026-07-21 19:38:59 -0500

    add swiper animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,6 +193,7 @@ reveal('.work__data .section__title', {})
 reveal('.work__description', { delay: .6 })
 reveal('.work__data .swiper-pagination', { delay: .9 })
 reveal('.work__data :is(.swiper-button-prev, .swiper-button-next)', { delay: 1.2 })
+reveal('.work__swiper', { delay: .9 })
 
 /* Service animation */
 
